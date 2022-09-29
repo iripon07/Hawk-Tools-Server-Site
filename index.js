cc602e08783a492d03ef78c588fa21c736cd72e3
AsadulIslamRipon <mdasadulripon@gmail.com>
2022-09-30 04:02:49 +0600

    finseddd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -16,11 +17,50 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const toolsCollection = client.db("hawktools").collection("tools");
     const reviewsCollection = client.db("hawktools").collection("reviews");
+    const ordersCollection = client.db("hawktools").collection("orders");
+    const usersCollection = client.db("hawktools").collection("users");
+
+    //AUTH
+
+    // app.post("/signin", async(req, res) => {
+    //   const user = req.body;
+    //   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+    //   res.send(accessToken);
+    // })
+
+    //Post user
+    app.put("/users/:email", async(req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+      res.send({result, token});
+    })
 
     //GET ALL TOOLS
     app.get("/tools", async (req, res) => {
@@ -32,7 +72,8 @@ async function run() {
 
 
     app.get("/tools/:id", async (req, res) => {
-      const id = req.params.id;
+      const id = req.params;
+      // console.log(id);
       const query = { _id: ObjectId(id) };
       const result = await toolsCollection.findOne(query);
       res.send(result);
@@ -47,6 +88,28 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     })
+
+
+
+    //Order
+
+    //Order Post
+    app.post("/orders", async( req, res) => {
+      const body = req.body;
+      const orders = await ordersCollection.insertOne(body);
+      res.send(orders);
+    })
+
+    //get orders by email
+    app.get("/orders", async(req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = {email: email}
+      const cursor = ordersCollection.find(query);
+      const orders =  await cursor.toArray();
+      res.send(orders);
+    })
+
   } finally {
   }
 }
